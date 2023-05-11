5135e65938f787f70bb943c53718ebcdef30456f
Jin-SukKim <99wlstjr@gmail.com>
2023-05-12 03:02:40 +0900

    link player data with hud ui and update

--- 53_code.cpp
+++ 53_code.cpp
@@ -174,4 +174,169 @@
             }
         }
     }
+
+
+    CharacterStat에 NPC를 위한 경험치 값 설정,
+    PlayerState에 플레이어 경험치 데이터 보관하도록 설계를 확장,
+    캐릭터 사망 시 NPC가 플레이어에게 죽는지 검사하고 해당 PlayerController를 통해 PlayerState를 업데이트하는 로직을 추가
+    ABPlayerState.h
+    ...
+        // 경험치
+        float GetExpRatio() const;
+        bool AddExp(int32 IncomeExp);
+        ...
+        UPROPERTY(Transient)
+        int Exp;
+
+    private:
+        void SetCharacterLevel(int32 NewCharacterLevel);
+        struct FABCharacterData* CurrentStatData;
+    ...
+    ABPlayerState.cpp
+    ...
+    AABPlayerState::AABPlayerState()
+    {
+        CharacterLevel = 1;
+        GameScore = 0;
+        Exp = 0;
+    }
+    void AABPlayerState::InitPlayerData()
+    {
+        SetPlayerName(TEXT("Destiny"));
+        SetCharacterLevel(5);
+        GameScore = 0;
+        Exp = 0;
+    }
+
+    float AABPlayerState::GetExpRatio() const
+    {
+        // 얻을 수 있는 Exp가 0에 가까운 경우
+        if (CurrentStatData->NextExp <= KINDA_SMALL_NUMBER)
+            return 0.f;
+
+        // UI의 ProgressBar에 표시되므로 Ratio 계산
+        float Result = (float)Exp / (float)CurrentStatData->NextExp;
+        ABLOG(Warning, TEXT("Ratio : %f, Current : %d, Next : %d"), Result, Exp, CurrentStatData->NextExp);
+        return Result;
+    }
+
+    bool AABPlayerState::AddExp(int32 IncomeExp)
+    {
+        if (CurrentStatData->NextExp == -1)
+            return false;
+
+        bool DidLevelUp = false;
+        // 현재 Exp에 얻은 Exp 더해주기
+        Exp = Exp + IncomeExp;
+        // 충분한 경험치를 얻었다면 레벨업
+        if (Exp >= CurrentStatData->NextExp)
+        {
+            Exp -= CurrentStatData->NextExp;
+            SetCharacterLevel(CharacterLevel + 1);
+            DidLevelUp = true;
+        }
+
+        OnPlayerStateChanged.Broadcast();
+        return DidLevelUp;
+    }
+
+    void AABPlayerState::SetCharacterLevel(int32 NewCharacterLevel)
+    {
+        auto ABGameInstance = Cast<UABGameInstance>(GetGameInstance());
+        ABCHECK(nullptr != ABGameInstance);
+
+        CurrentStatData = ABGameInstance->GetABCHaracterData(NewCharacterLevel);
+        ABCHECK(nullptr != CurrentStatData);
+
+        CharacterLevel = NewCharacterLevel;
+    }
+
+    ABHUDWidget.cpp
+    ...
+    void UABHUDWidget::UpdatePlayerState()
+    {
+        ABCHECK(CurrentPlayerState.IsValid());
+
+        ExpBar->SetPercent(CurrentPlayerState->GetExpRatio());
+        PlayerName->SetText(FText::FromString(CurrentPlayerState->GetPlayerName()));
+        PlayerLevel->SetText(FText::FromString(FString::FromInt(CurrentPlayerState->GetCharacterLevel())));
+        CurrentScore->SetText(FText::FromString(FString::FromInt(CurrentPlayerState->GetScore())));
+    }
+    ...
+    ABCharacterStatComponent.h
+    ...
+    int32 GetDropExp() const;
+    ...
+    ABCharacterStatComponent.cpp
+    ...
+    int32 UABCharacterStatComponent::GetDropExp() const
+    {
+        return CurrentStatData->DropExp;
+    }
+    ...
+    ABCharacter.h
+    ...
+    int32 GetExp() const;
+    ...
+    ABCharacter.cpp
+    ...
+    int32 AABCharactter::GetExp() const
+    {
+        return CharacterStat->GetDropExp();
+    }
+
+    float AABCharactter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+    {
+        float FinalDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+        ABLOG(Warning, TEXT("Actor : %s took Damage : %f"), *GetName(), FinalDamage);
+
+        // StatComponent 클래스를 사용해 대미지 계산
+        CharacterStat->SetDamage(FinalDamage);
+
+        if (CurrentState == ECharacterState::DEAD)
+        {
+            // 대미지 framework에서 PlayerController의 정보는 가해자(Instigator) 인자로 전달
+            // 즉, 현재 캐릭터 액터가 NPC인 경우이다.
+            if (EventInstigator->IsPlayerController())
+            {
+                auto PlayerController = Cast<AABPlayerController>(EventInstigator);
+                ABCHECK(nullptr != PlayerController, 0.f);
+                PlayerController->NPCKill(this);
+            }
+        }
+        return FinalDamage;
+    }
+    ...
+    ABPlayerController.h
+    ...
+    // 플레이어가 NPC를 죽였는지 확인
+	void NPCKill(class AABCharacter* KilledNPC) const;
+    ...
+    UPROPERTY()
+	class AABPlayerState* ABPlayerState;
+    ...
+    ABPlayerController.cpp
+    ...
+    void AABPlayerController::BeginPlay()
+    {
+        Super::BeginPlay();
+
+        FInputModeGameOnly InputMode;
+        SetInputMode(InputMode);
+
+        HUDWidget = CreateWidget<UABHUDWidget>(this, HUDWidgetClass);
+        HUDWidget->AddToViewport();
+
+        // HUD위젯과 PlayerState 연결
+        ABPlayerState = Cast<AABPlayerState>(PlayerState);
+        ABCHECK(nullptr != ABPlayerState);
+        HUDWidget->BindPlayerState(ABPlayerState);
+        ABPlayerState->OnPlayerStateChanged.Broadcast();
+    }
+    
+    void AABPlayerController::NPCKill(AABCharactter* KilledNPC) const
+    {
+        ABPlayerState->AddExp(KilledNPC->GetExp());
+    }
+
 */
--- 53_link_data_with_hud.cpp
+++ 53_link_data_with_hud.cpp
@@ -25,5 +25,18 @@
     
     code.cpp (ABPlayerController.cpp, ABCharacter.cpp from line 146)
 
-    
+    플레이어 데이터에경험치 정보를 표시하도록 기능을 추가한다.
+    경험치 획득과 레벨업 기능을 구현한다.
+
+    UI에서는 해당 정보를 ProgressBar로 표시하기에 경험치와 해당 레벨의 최대 경험치의 비율 정보를 구하는 작업이 필요하다.
+
+    CharacterStat에 NPC를 위한 경험치 값 설정,
+    PlayerState에 플레이어 경험치 데이터 보관하도록 설계를 확장,
+    캐릭터 사망 시 NPC가 플레이어에게 죽는지 검사하고 해당 PlayerController를 통해 PlayerState를 업데이트하는 로직을 추가해줘야 한다.
+
+    대미지 framework에서 PlayerController의 정보는 가해자(Instigator) 인자로 전달되므로 이를 사용하면 수월해진다.
+
+    code.cpp (ABPlayerState class, from line 179)
+
+    코드를 적용하면 경험치 관련 항목이 업데이트 된걸 확인할 수 있다.
 */
